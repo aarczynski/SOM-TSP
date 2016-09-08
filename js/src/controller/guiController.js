daf67860039be493dce2b87c03b95452d2c64463
Adam Arczynski <adam.arczynski@gmail.com>
2016-09-08 10:16:33 +0200

    replace setInterval with requestAnimationFrame

--- js/src/controller/guiController.js
+++ js/src/controller/guiController.js
@@ -1,20 +1,14 @@
 App.Controllers.GUIController = {
     registerStartListener: function () {
-        window.main = function () {
-            setInterval(function() {
-                App.Controllers.HUDController.refresh();
-                App.Controllers.CanvasController.refresh();
-            }, 1);
-        };
-
         $('#start').click(function() {
             App.main.init();
-            main();
             update();
 
             function update() {
                 for (var i = 0; i < 50; i++) {App.main.step();}
-                setTimeout(update, 1)
+                App.Controllers.HUDController.refresh();
+                App.Controllers.CanvasController.refresh();
+                requestAnimationFrame(update);
             }
         });
     }
